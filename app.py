fbc54a8a597207d8e66bdd2e1aeae93972e89e7c
Dhruv Singh <dhruv0607singh@gmail.com>
2022-06-08 14:40:15 +0530

    Shift local port to Heroku cloud

--- app.py
+++ app.py
@@ -28,8 +28,10 @@ class ColorCommand(Resource):
 api.add_resource(ColorCommand, '/commandC')  #http:127.0.0.1:5000/commandC - post and get
 api.add_resource(Home, '/')
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
+# port = int(os.environ.get("PORT", 5000))
+# app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
 '''
 TODO: 
